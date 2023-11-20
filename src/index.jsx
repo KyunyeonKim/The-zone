7f51fb78d185b27d0a3ad5a3b9ae2fb1a3304e4d
wonyong92 <ska40806@gmail.com>
2023-11-20 17:31:35 +0900

    chun branch

--- src/index.jsx
+++ src/index.jsx
@@ -1,35 +1,9 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from 'react';
 
-import './index.css'
-
-import MainEndPoint from "./jang/MainEndPoint";
-import GetHistoryOfVacationDefaultSetting from "./component/kim/component/GetHistoryOfVacationDefaultSetting";
-import UpdateEmployee from "./component/kim/component/UpdateEmployee";
-import CreateEmployee from "./component/kim/component/CreateEmployee";
-import SetWorkTime from "./component/kim/component/SetWorkTime";
-import GetVacationHistory from "./component/kim/component/GetVacationHistory";
-import PostSetWorkTime from "./component/kim/component/PostSetWorkTime";
-import EmployeeMine from "./component/kim/component/EmployeeMine";
-import VacationDashboard from "./component/kim/component/DashBoard/VacationDashBoard";
-import EmployeeReport from "./component/kim/component/EmployeeReportDashBoard/EmployeeReport";
+import EmployeeVacationSetting from "./component/chun/Component/EmployeeVacationSetting";
+import {render} from "react-dom";
 
 
-export const stateStore=[]
-document.addEventListener('DOMContentLoaded', function() {
-  render(
-
-    // <MainEndPoint/>,
-    //   <GetHistoryOfVacationDefaultSetting/>,
-    //   <UpdateEmployee/>,
-    //   <SetWorkTime/>,
-      // <CreateEmployee/>,
-
-      // <GetVacationHistory/>,
-      // <PostSetWorkTime/>,
-      // <EmployeeMine/>,
-      // <VacationDashboard/>,
-      <EmployeeReport/>,
-    document.body.appendChild(document.createElement('div'))
-  )
+document.addEventListener('DOMContentLoaded', function () {
+    render(<EmployeeVacationSetting/>, document.body.appendChild(document.createElement('div')))
 })
