5e5395a0d100292651d0b7ed9964a327a3bef85d
wonyong92 <ska40806@gmail.com>
2023-11-20 17:32:08 +0900

    kim branch

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,7 @@
 import React from 'react';
-import EmployeeVacationSetting from "./component/chun/Component/EmployeeVacationSetting";
+import CreateEmployee from "./component/kim/component/CreateEmployee";
 import {render} from "react-dom";
-
-
-import "./index.css"
-import AppealRequest from "./component/chun/Component/AppealRequest";
-import AttendanceApprovalAllEmployees from "./component/chun/Component/AttendanceApprovalAllEmployees";
-import AttendanceApprovalEmployee from "./component/chun/Component/AttendanceApprovalEmployee";
-import ProcessAppealRequest from "./component/chun/Component/ProcessAppealRequest";
-import VacationDefaultSetting from "./component/chun/Component/VacationDefaultSetting";
-import VacationProcess from "./component/chun/Component/VacationProcess";
-import VacationRequest from "./component/chun/Component/VacationRequest";
-import ProcessAppealRequestListComponent from "./component/chun/Component/ProcessAppealRequestListComponent";
-
-//VacationProcess
 document.addEventListener('DOMContentLoaded', function () {
-    render(<VacationRequest/>, document.body.appendChild(document.createElement('div')))
+    render(<CreateEmployee/>, document.body.appendChild(document.createElement('div')))
 })
+
