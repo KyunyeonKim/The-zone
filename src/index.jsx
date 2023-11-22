e04f1ea6b235da1fb5b5d9a9306ebd7d31ecc8b7
KyunyeonKim <kgn4746@naver.com>
2023-11-22 13:51:17 +0900

    SetWorkTime()근무시간조정내역_구현완료

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,24 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import './index.css'
+
 import MainEndPoint from "./jang/MainEndPoint";
 import GetHistoryOfVacationDefaultSetting from "./component/kim/component/GetHistoryOfVacationDefaultSetting";
+import UpdateEmployee from "./component/kim/component/UpdateEmployee";
+import CreateEmployee from "./component/kim/component/CreateEmployee";
+import SetWorkTime from "./component/kim/component/SetWorkTime";
+
 
 
 export const stateStore=[]
 document.addEventListener('DOMContentLoaded', function() {
   render(
+
     // <MainEndPoint/>,
-      <GetHistoryOfVacationDefaultSetting/>,
+    //   <GetHistoryOfVacationDefaultSetting/>,
+      // <UpdateEmployee/>,
+      <SetWorkTime/>,
+      // <CreateEmployee/>,
     document.body.appendChild(document.createElement('div'))
   )
 })
