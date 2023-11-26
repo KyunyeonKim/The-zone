6588dc1958ec8c7b51be9f76a98c670eee79eda9
천아현 <fofi7889@naver.com>
2023-11-26 13:24:17 +0900

    [FEAT] ListComponent 컴포넌트 작성

---
+++ src/component/chun/Component/ListComponent.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import {withStyles} from "@material-ui/core/styles";
+
+
+const styles = (theme) => ({
+    tableCell:{
+        fontSize:'1.2rem',
+        textAlign: 'center'
+    }
+
+});
+
+class ListComponent extends Component {
+    constructor(props) {
+        super(props);
+        // this.state = {
+        //     employeeId: "",
+        // };
+    }
+
+    render() {
+        /*고유적으로 식별할 수 있는 keyData의 데이터를 받아옴*/
+        const {row,keyData,classes} = this.props;
+
+        // const onButtonClick=async(e)=>{
+        //     await this.setState({employeeId:row.employeeId});
+        //     console.log("employeeId : ",row.employeeId);
+        //     console.log("this.state : ",this.state);
+        // }
+
+        return (
+            <TableRow key={keyData}>
+                {Object.entries(row).map(([key, value]) => (
+                    <TableCell key={key} className={classes.tableCell}>
+                        {value}
+                    </TableCell>
+                ))}
+            </TableRow>
+
+        );
+    }
+}
+export default withStyles(styles)(ListComponent);
