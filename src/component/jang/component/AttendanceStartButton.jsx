349a7888497a14be559ee77c69ead80df3bd6b11
wonyong92 <ska40806@gmail.com>
2023-12-13 10:34:34 +0900

    출근 입력, 퇴근 입력을 동적으로 변경 - 정보가 있는 경우 자동으로 출근 시간, 퇴근 시간으로 출력 버튼 1에 근태 보고서 연결

--- src/component/jang/component/AttendanceStartButton.jsx
+++ src/component/jang/component/AttendanceStartButton.jsx
@@ -4,9 +4,10 @@ import {
     Dialog, DialogActions, DialogContent, DialogTitle,
 } from '@material-ui/core';
 import axios from "axios";
+import Typography from "@material-ui/core/Typography";
 
 class AttendanceStartButton extends Component {
-    state = {dialogOn :false ,dialogOff:true}
+    state = {dialogOn :false ,dialogOff:true,startTime:null}
     dialogOn=false
     dialogOff=true
     dialogShowToggle = () => {
@@ -22,18 +23,56 @@ class AttendanceStartButton extends Component {
         try{
             let response = await axios.post('http://localhost:8080/employee/attendance')
             alert("업무 시작!!")
-            this.setState({dialogOn :false ,dialogOff:true})
+            axios.defaults.withCredentials=true;
+            let responseOfTodayInfo = await axios.get('http://localhost:8080/employee/attendance/today')
+            let {startTime} = responseOfTodayInfo.data;
+            alert('startTime '+ responseOfTodayInfo.data.startTime)
+            if(startTime!=="null")
+                this.setState({startTime:startTime,dialogOn :false ,dialogOff:true})
+
         }catch(error) {
             alert(`출근 요청 도중 문제 발생 : ${error.response.data.message}`)
-            this.setState({dialogOn :false ,dialogOff:true})
+            axios.defaults.withCredentials=true;
+            let response = await axios.get('http://localhost:8080/employee/attendance/today')
+            let {startTime} = response.data;
+            alert('startTime '+ response.data.startTime)
+            if(startTime!=="null")
+                this.setState({startTime:startTime,dialogOn :false ,dialogOff:true})
         }
     }
 
+    async componentDidMount() {
+        axios.defaults.withCredentials=true;
+        let response = await axios.get('http://localhost:8080/employee/attendance/today')
+        let {startTime} = response.data;
+        if(startTime!=="null")
+            this.setState({startTime:startTime})
+        else{
+            alert("출근 정보 없음")
+        }
+    }
     render() {
+        let startTime=null;
+
+        if(this.state.startTime!==null){
+            let currentDate = new Date(this.state.startTime);
+            let hours = currentDate.getHours().toString().padStart(2, '0');
+            let minutes = currentDate.getMinutes().toString().padStart(2, '0');
+            let seconds = currentDate.getSeconds().toString().padStart(2, '0');
+            startTime =hours + ':' + minutes + ':' + seconds;
+        }
+        alert(`start time ${this.state.startTime}`)
+
+
         return (<div>
-            <Button variant="contained" onClick={this.dialogShowToggle} style={{backgroundColor:"#FFCA6E",width:'110px',height:'40px',fontFamily:'IBM Plex Sans KR',fontSize:'17px',borderRadius:'20px',fontWeight:'bold'}}>
-                출근 입력
-            </Button>
+            {startTime===null?<Button variant="contained" color="primary" onClick={this.dialogShowToggle} style={{backgroundColor:"#FFCA6E",width:'110px',height:'40px',fontFamily:'IBM Plex Sans KR',fontSize:'17px',borderRadius:'20px',fontWeight:'bold'}}>출근 입력</Button>
+            :<Typography>
+                {
+                    startTime
+                }
+            </Typography>}
+
+
             <Dialog open={this.dialogOn} onClose={this.dialogShowToggle} aria-labelledby="update-modal-title">
                 <DialogTitle id="update-modal-title">업무 시작</DialogTitle>
                 <DialogContent>
--- src/index.jsx
+++ src/index.jsx
@@ -5,29 +5,20 @@ import './index.css'
 import MainEndPoint from "./component/jang/page/MainEndPoint";
 import ErrorBoundary from "./component/jang/page/ErrorBoundary";
 
-import EmployeeDashboard from "./component/kim/ModalPage/EmployeeDashboard";
-import PostSetWorkTime from "./component/kim/ModalPage/PostSetWorkTime";
-import CreateEmployee from "./component/kim/ModalPage/CreateEmployee";
-import VacationDashboard from "./component/kim/ModalPage/VacationDashBoard";
-export const stateStore={}
+export const stateStore = {}
 
-export const chartDataStore = {store: {
-    '1': [], '2': [], '3': [],
-    '4': [], '5': [], '6': [],
-    '7': [], '8': [], '9': [],
-    '10': [], '11': [], '12': []
-  }, appendData: (store,data, month) => {
-    store[month]={...store[month],...data}//이전에 있던 data, 새로 입력된 data를 merge
-  },updateChart:undefined}
+export const chartDataStore = {
+    store: {
+        '1': [], '2': [], '3': [], '4': [], '5': [], '6': [], '7': [], '8': [], '9': [], '10': [], '11': [], '12': []
+    }, appendData: (store, data, month) => {
+        store[month] = {...store[month], ...data}//이전에 있던 data, 새로 입력된 data를 merge
+    }, updateChart: undefined
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-  render(
-      <ErrorBoundary>
-          <MainEndPoint/>
-      </ErrorBoundary>,
-    //   <EmployeeDashboard/>,
-    //   <CreateEmployee/>,
-
-    document.body.appendChild(document.createElement('div'))
-  )
+document.addEventListener('DOMContentLoaded', function () {
+    render(<>
+        <ErrorBoundary>
+            <MainEndPoint/>
+        </ErrorBoundary>
+    </>, document.body.appendChild(document.createElement('div')))
 })
